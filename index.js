2cc9934a093849e0a19aef4bb9cda30637c42879
Moshe Kolodny <mkolodny@google.com>
2017-12-05 10:48:25 -0500

    Revert polyfill and add a wrapper for the real nextTick.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var usePolyfill = !process.version ||
+if (!process.version ||
     process.version.indexOf('v0.') === 0 ||
-    process.version.indexOf('v1.') === 0 && process.version.indexOf('v1.8.') !== 0;
+    process.version.indexOf('v1.') === 0 && process.version.indexOf('v1.8.') !== 0) {
+  module.exports = nextTickPolyfill;
+} else {
+  module.exports = nextTickWrapper;
+}
 
-module.exports = nextTick;
-
-function nextTick(fn, arg1, arg2, arg3) {
+function nextTickPolyfill(fn, arg1, arg2, arg3) {
   if (typeof fn !== 'function') {
     throw new TypeError('"callback" argument must be a function');
   }
@@ -17,27 +19,18 @@ function nextTick(fn, arg1, arg2, arg3) {
   case 1:
     return process.nextTick(fn);
   case 2:
-    return usePolyfill ?
-      process.nextTick(function afterTickOne() {
-        fn.call(null, arg1);
-      }) :
-      process.nextTick(fn, arg1);
+    return process.nextTick(function afterTickOne() {
+      fn.call(null, arg1);
+    });
   case 3:
-    return usePolyfill ?
-      process.nextTick(function afterTickTwo() {
-        fn.call(null, arg1, arg2);
-      }) :
-      process.nextTick(fn, arg1, arg2);
+    return process.nextTick(function afterTickTwo() {
+      fn.call(null, arg1, arg2);
+    });
   case 4:
-    return usePolyfill ?
-      process.nextTick(function afterTickThree() {
-        fn.call(null, arg1, arg2, arg3);
-      }) :
-      process.nextTick(fn, arg1, arg2, arg3);
+    return process.nextTick(function afterTickThree() {
+      fn.call(null, arg1, arg2, arg3);
+    });
   default:
-    if (!usePolyfill) {
-      return process.nextTick.apply(null, arguments);
-    }
     args = new Array(len - 1);
     i = 0;
     while (i < args.length) {
@@ -46,5 +39,21 @@ function nextTick(fn, arg1, arg2, arg3) {
     return process.nextTick(function afterTick() {
       fn.apply(null, args);
     });
+  }
+}
+
+function nextTickWrapper(fn, arg1, arg2, arg3) {
+  switch (arguments.length) {
+  case 0:
+  case 1:
+    return process.nextTick(fn);
+  case 2:
+    return process.nextTick(fn, arg1);
+  case 3:
+    return process.nextTick(fn, arg1, arg2);
+  case 4:
+    return process.nextTick(fn, arg1, arg2, arg3);
+  default:
+    return process.nextTick.apply(process, arguments);
   }
 }
